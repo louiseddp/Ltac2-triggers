a941a2521625cea6017aec5b4b33f7d8e1ef19e3
lduboisd <louise.dubois-de-prisque@inria.fr>
2023-11-30 17:32:06 +0100

    not conclusing tests

--- orchestrator/Tactics.v
+++ orchestrator/Tactics.v
@@ -4,15 +4,16 @@ From Ltac2 Require Import Constr.
 Require Import Printer.
 Require Import Triggers.
 
-Ltac2 get_opt o := match o with None => Control.throw Not_found | Some x => x end.
-
-(** The tactics should be saved in a specific file because we need the absolute path **)
+(** The tactics should be saved in a specific file because 
+[Ltac1.ref] needs the absolute path *)
 
 Ltac mysplit := split.
 Ltac myexact t := exact t.
 Ltac myapply2 A B := split ; [apply A | apply B].
 
-(** Reification of Ltac1 tactics **)
+Tactic Notation "mysplit2" := mysplit.
+
+(** Reification of Ltac1 tactics *)
 
 Ltac2 currify (tac : t) (l : t list) :=
   Ltac1.apply tac l run.
@@ -23,25 +24,61 @@ Ltac2 constr_to_tacval (l : constr list) :=
 Ltac2 apply_ltac1 (tac : t) (l : constr list) := 
 currify tac (constr_to_tacval l).
 
-Ltac2 run (s : string) (l : constr list) :=
-let ident := Ident.of_string s in
-let tac := ref [ident:(Orchestrator) ; ident:(Tactics); get_opt ident] in
+Ltac2 get_opt o := match o with None => Control.throw Not_found | Some x => x end.
+
+(** [run] runs a Ltac1 tactic *) 
+
+Ltac2 run (id : ident) (l : constr list) :=
+let tac := ref [ident:(Orchestrator) ; ident:(Tactics); id] in
 let l := List.map of_constr l in
 let l := of_list l in
 ltac1:(tac l |- 
 let f := ltac2:(tac l |- 
-let l := Ltac1.to_list l in 
+let l := to_list l in 
 let l := get_opt l in
 let l := List.map (fun x => get_opt (to_constr x)) l in
 apply_ltac1 tac l) in f tac l) tac l.
 
+(* Ltac2 run (tac : t) (l : constr list) :=
+let l := List.map of_constr l in
+let l := of_list l in
+ltac1:(tac l |- 
+let f := ltac2:(tac l |- 
+let l := to_list l in 
+let l := get_opt l in
+let l := List.map (fun x => get_opt (to_constr x)) l in
+apply_ltac1 tac l) in f tac l) tac l.
+
+Ltac2 Notation "run" tac(tactic) := run ltac1val:(tac).
+ *)
+(* Ltac2 run_ltac1val2 (tac : t) (l : t) :=
+ltac1:(tac l |- 
+let f := ltac2:(tac l |- 
+let l := to_list l in 
+let l := get_opt l in
+let l := List.map (fun x => get_opt (to_constr x)) l in
+apply_ltac1 tac l) in f tac l) tac l.
+
+Tactic Notation (at level 0) "run" ltac(tac) "with" hyp_list(l) := 
+idtac 1 ;
+let f :=
+ltac2:(tac l |- 
+run_ltac1val2 (ltac1val:(tac |- tac) tac) l) in 
+f ltac:(tac) constr:(l). *)
+
+
+Section tests. 
 
 Goal (True /\ True) /\ (True -> True -> True /\ True).
-run "mysplit" [].
-run "mysplit" [].
-run "myexact" ['I].
-run "myexact" ['I].
-intros H1 H2.
-run "myapply2" ['H1; 'H2]. Qed.
+Proof.
+run @mysplit [].
+run @mysplit [].
+(* run @mysplit2 []. Uncatchable exception: ref does not work with Tactic Notation *)
+run @myexact ['I].
+run @myexact ['I].
+intros H1 H2. 
+run @myapply2 ['H1; 'H2]. Qed.
+
+End tests.
 
 
